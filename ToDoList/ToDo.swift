d0cccd4d77cafb1b5c8fbd785e2d0806e195e2f4
Marina Lee <leemarina@gmail.com>
2021-07-19 14:33:41 -0700

    todo class

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Marina Lee on 7/19/21.
+//  Copyright © 2021 marinalee. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
