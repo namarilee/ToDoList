2f347747093e0454f4ca22f978d274a538b52a24
Marina Lee <leemarina@gmail.com>
2021-07-19 14:54:01 -0700

    tableView func

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -9,38 +9,45 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
+    
+    var toDos: [ToDo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        toDos = createToDos()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        
+        return [swift, dog]
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return toDos.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
+         //Configure the cell...
+        let toDo = toDos[indexPath.row]
+        cell.textLabel?.text = toDo.name
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
